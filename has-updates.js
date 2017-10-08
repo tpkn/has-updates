05e6f73e118bd19429cec8590baf03c3eadf9835
tpkn <tpkn@users.noreply.github.com>
2017-10-08 23:06:15 +0300

    Add files via upload

--- has-updates.js
+++ has-updates.js
@@ -1,10 +1,11 @@
 /*!
- * Has Updates (v1.1.8.20171001), http://tpkn.me/
+ * Has Updates (v1.1.10.20171008), http://tpkn.me/
  */
 
 const request = require('request');
+const compare = require('compare-version');
 
-module.exports = (link, version = '0.0.1') => {
+module.exports = (link, current_version = '0.0.1') => {
    return new Promise((resolve, reject) => {
       if(typeof link === 'undefined'){
          reject({type:'error', text: 'Missed link to updates'});
@@ -13,19 +14,21 @@ module.exports = (link, version = '0.0.1') => {
       request.get(link, (error, response, body) => {
          if(error){
             reject({type:'error', text: 'Connection problems'});
+            return;
          }
          if(response.statusCode != 200){
             reject({type:'error', text: 'No updates.json file'});
+            return;
          }
 
-         let data = JSON.parse(body);
-         if(data){
-            if(version != data.version){
+         try {
+        		let data = JSON.parse(body);
+            if(compare(data.version, current_version)){
                resolve({version: data.version, win: data.win, mac: data.mac, linux: data.linux});
             }else{
-               reject({type:'message', text: 'You have latest ' + version + ' version'});
+               reject({type:'message', text: 'You have latest ' + current_version + ' version'});
             }
-         }else{
+         }catch(e){
             reject({type:'error', text: 'Invalid json file'});
          }
       });
